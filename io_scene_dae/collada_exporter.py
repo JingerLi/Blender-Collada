b9ddd57304fac590d2f3722af42d972bff09383b
lijinger <miles_lee@hotmail.com>
2019-05-23 00:03:19 +0800

    Add XML module and write a COLLADA header.

--- io_scene_dae/collada_exporter.py
+++ io_scene_dae/collada_exporter.py
@@ -1,2 +1,9 @@
+import xml.etree.ElementTree as ET
+
 def export( context, filepath ):
-    print('Output to: ' + filepath)
+    collada = ET.Element('COLLADA')
+    collada.set('xmlns', 'http://www.collada.org/2005/11/COLLADASchema')
+    collada.set('version', '1.5.0')
+    collada.set('mlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+    tree = ET.ElementTree(collada)
+    tree.write(filepath, encoding="utf-8", xml_declaration=True)
